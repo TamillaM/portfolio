6888185aa2c305339833dfb757ee3d2a6fc34e7d
Tammilla <tamilla19820204@gmail.com>
2025-01-10 21:23:00 +0100

    update

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -7,7 +7,7 @@ const Certificate = () => {
   return (
     <div className='app__certificate' id='certificate'>
         <div className="app__certificate-title">
-            <p style={{fontSize:"15px", color:"rgb(123,123,123)"}}>Watch my</p>
+            <p style={{fontSize:"15px", color:"rgb(123,123,123)"}}>View my</p>
             <h1 style={{fontSize:"50px"}}>Certificate</h1>
         </div>
         <div className="app__certificate-info">
